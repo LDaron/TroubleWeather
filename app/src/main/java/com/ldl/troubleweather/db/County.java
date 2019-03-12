d1bb15fd4fb553c8e58fa46efaf3bcb8c1dddc74
LDaron <377438996@qq.com>
2019-03-13 00:24:16 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/ldl/troubleweather/db/County.java
@@ -0,0 +1,47 @@
+package com.ldl.troubleweather.db;
+
+/**
+ * 功能：
+ * 描述：
+ * time：2019/3/13
+ * Created by 李达龙
+ */
+public class County {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/ldl/troubleweather/db/Province.java
@@ -0,0 +1,39 @@
+package com.ldl.troubleweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 功能：
+ * 描述：
+ * time：2019/3/13
+ * Created by 李达龙
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
